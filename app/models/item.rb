3522fa03ec78a129d4514c6439a865d6b0358827
Francisco Mancilla <frmancil@hotmail.com>
2024-03-21 20:24:32 -0500

    active admin installed and item resource added

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+
+    def self.ransackable_attributes(auth_object = nil)
+        ["name", "stock", "price", "rarity"]
+      end
+    
+      def self.ransackable_associations(auth_object = nil)
+        ["name", "stock", "price", "rarity"]
+      end
 end
