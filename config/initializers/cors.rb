489dd6b593ec37a01d6e63773f04a276f587eb2f
Francisco Mancilla <frmancil@hotmail.com>
2024-04-16 17:41:51 -0500

    cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
     allow do
-      origins '*'
+      origins ['https://pokemagic-3945bd616b13.herokuapp.com']
   
       resource '*',
         headers: :any,
