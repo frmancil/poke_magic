8bf73553908d06610bf34c40e1ca3a29bc325d04
Francisco Mancilla <frmancil@hotmail.com>
2024-04-16 15:24:56 -0500

    migration fixes

--- db/migrate/20240322004028_create_items.rb
+++ db/migrate/20240322004028_create_items.rb
@@ -1,9 +1,6 @@
 class CreateItems < ActiveRecord::Migration[7.1]
   def change
     create_table :items do |t|
-      t.belongs_to :game_edition, index: true, foreign_key: true
-      t.belongs_to :game_type, index: true, foreign_key: true
-      t.belongs_to :item_type, index: true, foreign_key: true
       t.string :name
       t.integer :stock
       t.decimal :price
